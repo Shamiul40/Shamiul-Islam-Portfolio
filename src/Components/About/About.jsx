02f1efcb27eeb9696ba0eda04f086bc7036dc0fa
Shamiul Islam <shmaiul4040@gmail.com>
2025-08-11 17:41:06 +0600

    banner image edit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
 import Tilt from "react-parallax-tilt";
-import profileImage from "../../assets/profile5.png";
+import profileImage from "../../assets/profile7.png";
 
 const About = () => {
   return (
@@ -65,7 +65,7 @@ const About = () => {
         {/* Right Side */}
         <div className="md:w-1/2 flex justify-center md:justify-end">
           <Tilt
-            className="w-48 h-48 sm:w-64 sm:h-64 md:w-[25rem] md:h-[25rem] border-4 border-purple-700 rounded-full"
+            className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
             tiltMaxAngleX={20}
             tiltMaxAngleY={20}
             perspective={1000}
