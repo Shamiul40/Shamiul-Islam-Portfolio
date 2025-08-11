89ed326f7ba127fbae66685c585531c7bd8111ff
Shamiul Islam <shmaiul4040@gmail.com>
2025-08-11 16:57:49 +0600

    about section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import BlurBlob from '../BlurBlob'
 import './App.css'
 import About from './Components/About/About'
 import Contact from './Components/Contact/Contact'
@@ -14,6 +15,9 @@ function App() {
 
   return (
     <div className="bg-[#050414]">
+
+      <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
+
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]">
 
       </div>
