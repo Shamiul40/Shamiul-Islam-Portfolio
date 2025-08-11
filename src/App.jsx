d6d990556a7cb306dc94e26315f18bb24f5206ae
Shamiul40 <shamiul4040@gmail.com>
2025-08-11 11:06:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import './App.css'
+import About from './Components/About/About'
+import Contact from './Components/Contact/Contact'
+import Education from './Components/Education/Education'
+import Experience from './Components/Experience/Experience'
+import Footer from './Components/Footer/Footer'
+import Navbar from './Components/Navbar/Navbar'
+import Skills from './Components/Skills/Skills'
+import Work from './Components/Work/Work'
+
+function App() {
+  
+
+  return (
+    <div className="bg-[#050414]">
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]">
+
+      </div>
+      <div className='relative pt-20'>
+        <Navbar></Navbar>
+        <About></About>
+        <Skills></Skills>
+        <Experience></Experience>
+        <Work></Work>
+        <Education></Education>
+        <Contact></Contact>
+        <Footer></Footer>
+      </div>
+    </div>
+  )
+}
+
+export default App
